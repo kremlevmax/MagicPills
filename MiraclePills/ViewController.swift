0622727c0d1ab77a2a05ab8300022fb07b388f5a
kremlevmax <kremlevmax1989@gmail.com>
2017-03-04 00:46:23 +0200

    - Picker menu was added

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var buttonOfState: UIButton!
 
+    @IBOutlet weak var pickerOfState: UIPickerView!
+    
+    let states = ["Alaska", "Arizona", "Texas", "California", "Maimi"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerOfState.dataSource = self
+        pickerOfState.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+ 
+    @IBAction func buttonOfStatePressed(_ sender: UIButton) {
+        pickerOfState.isHidden = false
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        buttonOfState.setTitle(states[row], for: UIControlState.normal)
+        pickerOfState.isHidden = true
+    }
 
 }
 
